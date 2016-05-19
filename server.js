9493c92298bb8b141bde75dd6ebb394a50ecb8f2
Ubuntu <ubuntu@ip-172-31-60-170.ec2.internal>
2016-05-19 21:06:32 +0000

    update for https and dashboard

--- server.js
+++ server.js
@@ -2,10 +2,27 @@
  * The Elastrix Parse Server
  * Copyright 2016 Elastrix, all rights reserved
  **/
+require('shelljs/global');
+var instanceId = exec('ec2metadata --instance-id',{silent:true}).stdout;
+var ip = exec('ec2metadata --public-ipv4',{silent:true}).stdout;
+var host = exec('ec2metadata --public-hostname',{silent:true}).stdout;
 var express = require('express'),
+    path = require('path'),
+    fs = require('fs'),
     ParseServer = require('parse-server').ParseServer,
     ParseDashboard = require('parse-dashboard'),
-    app = express();
+    app = express(),
+    options = {},
+    certsPath = path.join(__dirname, 'certs', 'server'),
+    caCertsPath = path.join(__dirname, 'certs', 'ca');
+
+
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+if (!databaseUri) {
+    console.log('DATABASE_URI not specified, falling back to localhost.');
+    databaseUri = "mongodb://localhost:27017/dev";
+}
 
 /**
  * Edit these values to match your configuration
@@ -13,14 +30,14 @@ var express = require('express'),
  * elastrix parse server
  */
 var port = process.env.port || 1337,
-    appId = 'elastrix',
-    appName = 'elastrix-parse',
-    masterKey = 'elastrix-parse',
-    fileKey = 'optionalFileKey',
-    serverURL = 'http://localhost:'+port+'/parse',
-    mongoURL = 'mongodb://localhost:27017/dev',
-    cloud = '/home/ubuntu/parse/cloud/main.js',
-    allowInsecureHTTP = 1;  
+    appId = process.env.APP_ID || 'elastrix',
+    appName = process.env.APP_NAME || 'elastrix',
+    masterKey = process.env.MASTER_KEY || 'elastrix',
+    fileKey = process.env.FILE_KEY || '',
+    serverURL = process.env.SERVER_URL || 'https://'+ip+':'+port+'/parse',
+    mongoURL = databaseUri,
+    cloud = process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    allowInsecureHTTP = process.env.ALLOW_INSECURE_HTTP || 0;  
 /**
  * The API server just uses the values from above,
  * you shouldn't need to modify this
@@ -31,8 +48,14 @@ var api = new ParseServer({
     appId: appId,
     masterKey: masterKey,
     fileKey: fileKey,
-    serverURL: serverURL
+    serverURL: serverURL,
+    liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
 });
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
 
 /** 
  * You can add more apps here if you want to
@@ -45,12 +68,46 @@ var dashboard = new ParseDashboard({
 	  serverURL: serverURL,
 	  appId: appId,
 	  masterKey: masterKey,
-	  appName: appName
+	  appName: appName,
+	  production: true
 	}
-    ]
+    ],
+    users: [
+	{
+	    user: "elastrix",
+	    pass: "elastrix"
+	}
+    ] 
 });
 
 app.use('/parse', api);
 app.use('/dashboard',dashboard);
-var httpServer = require('http').createServer(app);
-httpServer.listen(port);
+
+/**
+ * This is a simple route for the monitoring service
+ * to test. If this fails, monit will try and 
+ * restore the server.
+ */
+app.get('/elx', function(req, res) {
+  res.status(200).send('Elastrix Parse Server');
+});
+
+options = {
+  hostname: host,
+  key: fs.readFileSync(path.join(certsPath, 'my-server.key.pem'))
+  // This certificate should be a bundle containing your server certificate and any intermediates
+  // cat certs/cert.pem certs/chain.pem > certs/server-bundle.pem
+, cert: fs.readFileSync(path.join(certsPath, 'my-server.crt.pem'))
+  // ca only needs to be specified for peer-certificates
+//, ca: [ fs.readFileSync(path.join(caCertsPath, 'my-root-ca.crt.pem')) ]
+, requestCert: false
+, rejectUnauthorized: true
+};
+
+var httpServer = require('https').createServer(options,app);
+    httpServer.listen(port, function() {
+	console.log('elastrix-parse running on port ' + port + ' for instance-id: ' + instanceId + '.');
+    	console.log('elastrix-parse appId: ' + appId + '.');
+    });
+
+ParseServer.createLiveQueryServer(httpServer);
